a359255f05fcb4013cf8d72113130e0f5aa919c7
Damian Ponce <dponcez_git@hotmail.com>
2023-09-09 19:32:32 -0500

    Modify Game component: add (handlePlay) function

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import { Board } from '../Board/Board';
+import { Button  } from '../Button/Button'
 
 const Game = () => {
-  const [currentMove, setCurrentMove] = useState(0);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+  const spaces = [Array(9).fill(null)]
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [history, setHistory] = useState(spaces);
+  const xIsNext = currentIndex % 2 === 0;
+  const currentSquares = history[currentIndex];
 
   const handlePlay = (nextSquare) => {
     const nextHistory = [...history.slice(0, currentSquares + 1), nextSquare];
     setHistory(nextHistory)
-    setCurrentMove(nextHistory.length - 1);
+    setCurrentIndex(nextHistory.length - 1);
   }
+
+  const jumpToNext = (nextMove) => setCurrentIndex(nextMove);
+
+  const moves = history.map((squares, move) => {
+    let description;
+
+    move > 0 ? 
+      (description = `Go to move: ${move}`) :
+      (description = `Go to game start`)
+
+    return (
+      <li key={move}>
+        <Button
+          className="jump"
+          value={description}
+          onClick={() => jumpToNext(move)}
+        />
+      </li>
+    )
+  })
 
   return (
     <div className='game'>
@@ -22,6 +44,9 @@ const Game = () => {
           onPlay={handlePlay}
         />
       </div>
+      <ol className="game--list">
+        {moves}
+      </ol>
     </div>
   )
 }
